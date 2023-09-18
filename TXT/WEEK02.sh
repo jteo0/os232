c8f623bada024228bf54e27a6e55b492d16ebf83
jteo0 <114756781+jteo0@users.noreply.github.com>
2023-09-18 19:08:37 +0700

    Update WEEK02.sh

--- TXT/WEEK02.sh
+++ TXT/WEEK02.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while IFS= read -r line; do
-    grep '[0-9]{10}' inputSCRAP.txt >>outputSCRAP.txt
+    grep '[:digit:]{10}' inputSCRAP.txt >>outputSCRAP.txt
 done < "inputSCRAP.txt"
 
 
