8d4acb5dee66a4d85a4791421b5a4957168d00d4
jteo0 <114756781+jteo0@users.noreply.github.com>
2023-09-18 20:41:40 +0700

    Update WEEK02.sh

--- TXT/WEEK02.sh
+++ TXT/WEEK02.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-while IFS= read -r line; do
-    grep -o '/[[:digit:]]{10}/g' inputSCRAP.txt >>outputSCRAP.txt
-done < "inputSCRAP.txt"
+while IFS= read -r line; do grep -o '/[[:digit:]]{10}/g' inputSCRAP.txt >>outputSCRAP.txt; done < "inputSCRAP.txt"
